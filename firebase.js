26779154faaa5b0e2d0dddae36be0702f1b8aeb0
JasonRillera <jarillera@gmail.com>
2021-09-10 19:35:47 -0700

    update

--- firebase.js
+++ firebase.js
@@ -1,4 +1,4 @@
-import firebase from './firebase';
+import firebase from "firebase";
 
 const firebaseConfig = {
     apiKey: "AIzaSyBi-wg0RgEEiXClUR4vmFNQ0CU2hx8k8xM",
@@ -16,3 +16,30 @@ const app = !firebase.apps.length
 const db = app.firestore();
 
 export default db;
+
+
+
+
+
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// import { getFirestore } from "firebase/firestore"
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// const firebaseConfig = {
+//     apiKey: "AIzaSyBi-wg0RgEEiXClUR4vmFNQ0CU2hx8k8xM",
+//     authDomain: "v3-2697d.firebaseapp.com",
+//     projectId: "v3-2697d",
+//     storageBucket: "v3-2697d.appspot.com",
+//     messagingSenderId: "875966446979",
+//     appId: "1:875966446979:web:55f9c4b2e1898ad4674ab6"
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+
+// const db = getFirestore();
+
+// export default db;
